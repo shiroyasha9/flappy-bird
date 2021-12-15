40b3feb1c0c137dee22e967e69fabece2a26aec8
Mubin Ansari <mubinansari910@gmail.com>
2021-12-15 12:29:07 +0530

    add  comments

--- flappy.py
+++ flappy.py
@@ -1,15 +1,20 @@
 import pygame, sys
 
+# initialization
 pygame.init()
 
 screen = pygame.display.set_mode((576, 1024)) #width, height
 clock = pygame.time.Clock()
 
+# game loop
 while True:
+	# event loop
 	for event in pygame.event.get():
+		# if user quits
 		if event.type == pygame.QUIT:
 			pygame.quit() 
 			sys.exit()
 
+	# update the display with the max refresh rate being 120hz
 	pygame.display.update()
 	clock.tick(120)
