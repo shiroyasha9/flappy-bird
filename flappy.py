4ef89205606f323430f3512537236d3e30296257
Mubin Ansari <mubinansari910@gmail.com>
2022-02-04 23:04:07 +0530

    fix height and width issues finally

--- flappy.py
+++ flappy.py
@@ -2,18 +2,14 @@ import pygame, sys
 
 # draw the moving floor
 def draw_floor():
-	screen.blit(floor_surface, (floor_x_position,900))
-	screen.blit(floor_surface, (floor_x_position + width,900))
+	screen.blit(floor_surface, (floor_x_position,450))
+	screen.blit(floor_surface, (floor_x_position + width,450))
 
 # initialization
 pygame.init()
-# width = 576
-# height = 1024
-width = 432
-height = 768
-
+height = 512
+width = 288
 screen = pygame.display.set_mode((width, height)) #width, height
-
 clock = pygame.time.Clock()
 
 #Game variables
@@ -22,16 +18,13 @@ bird_movement = 0
 
 # background surface
 bg_surface = pygame.image.load('assets/background-day.png').convert()
-bg_surface = pygame.transform.scale2x(bg_surface)
 
 # floor surface
 floor_surface = pygame.image.load('assets/base.png').convert()
-floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_position = 0
 
 bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
-bird_surface = pygame.transform.scale2x(bird_surface)
-bird_rect = bird_surface.get_rect(center = (100,512))
+bird_rect = bird_surface.get_rect(center = (50,height/2))
 
 # game loop
 while True:
@@ -47,7 +40,7 @@ while True:
 			# if user presses spacebar while the game is active
 			if event.key == pygame.K_SPACE:
 				bird_movement = 0
-				bird_movement -= 8
+				bird_movement -= 5
 					
 	screen.blit(bg_surface, (0,0))
 
@@ -57,7 +50,7 @@ while True:
 	screen.blit(bird_surface, bird_rect)
 
 	# Floor
-	floor_x_position -= 1
+	floor_x_position -= 0.5
 	draw_floor()
 	if floor_x_position <= -width:
 		floor_x_position = 0
