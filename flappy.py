dbab3646bf4bc8bdb9e0bc13a23bd0a26fd14ff8
Mubin Ansari <mubinansari910@gmail.com>
2022-02-04 22:32:30 +0530

    fix width and height issues finally

--- flappy.py
+++ flappy.py
@@ -2,21 +2,21 @@ import pygame, sys, random
 
 # draw the moving floor
 def draw_floor():
-	screen.blit(floor_surface, (floor_x_position,900))
-	screen.blit(floor_surface, (floor_x_position + width,900))
+	screen.blit(floor_surface, (floor_x_position,450))
+	screen.blit(floor_surface, (floor_x_position + width,450))
 
 # create the top and bottom pipes
 def create_pipe():
 	random_pipe_position = random.choice(pipe_height)
-	bottom_pipe = pipe_surface.get_rect(midtop = (700, random_pipe_position))
-	top_pipe = pipe_surface.get_rect(midbottom = (700, random_pipe_position - 300))
+	bottom_pipe = pipe_surface.get_rect(midtop = (350, random_pipe_position))
+	top_pipe = pipe_surface.get_rect(midbottom = (350, random_pipe_position - 150))
 	return bottom_pipe, top_pipe
 
 # move the pipes and delete the pipes that are not visible
 def move_pipes(pipes):
 	for pipe in pipes:
-		pipe.centerx -= 5
-	visible_pipes = [pipe for pipe in pipes if pipe.right > -50]
+		pipe.centerx -= 3
+	visible_pipes = [pipe for pipe in pipes if pipe.right > -25]
 	return visible_pipes
 
 # draw the pipes on the screen
@@ -34,17 +34,15 @@ def check_collision(pipes):
 		if bird_rect.colliderect(pipe):
 			return False
 
-	if bird_rect.top <= -100 or bird_rect.bottom >= 900:
+	if bird_rect.top <= -50 or bird_rect.bottom >= 450:
 		return False
 	
 	return True
 
 # initialization
 pygame.init()
-# width = 576
-# height = 1024
-width = 432
-height = 768
+height = 512
+width = 288
 screen = pygame.display.set_mode((width, height)) #width, height
 clock = pygame.time.Clock()
 
@@ -55,24 +53,20 @@ game_active = True
 
 # background surface
 bg_surface = pygame.image.load('assets/background-day.png').convert()
-bg_surface = pygame.transform.scale2x(bg_surface)
 
 # floor surface
 floor_surface = pygame.image.load('assets/base.png').convert()
-floor_surface = pygame.transform.scale2x(floor_surface)
 floor_x_position = 0
 
 bird_surface = pygame.image.load('assets/bluebird-midflap.png').convert()
-bird_surface = pygame.transform.scale2x(bird_surface)
-bird_rect = bird_surface.get_rect(center = (100,384))
+bird_rect = bird_surface.get_rect(center = (50,height/2))
 
 # pipe surface and pipe userevent
 pipe_surface = pygame.image.load('assets/pipe-green.png').convert()
-pipe_surface = pygame.transform.scale2x(pipe_surface)
 pipe_list = []
 SPAWNPIPE = pygame.USEREVENT
 pygame.time.set_timer(SPAWNPIPE, 1200)
-pipe_height = [400, 500, 600,700]
+pipe_height = [200, 300, 350, 400]
 
 # game loop
 while True:
@@ -88,13 +82,13 @@ while True:
 			# if user presses spacebar while the game is active
 			if event.key == pygame.K_SPACE and game_active:
 				bird_movement = 0
-				bird_movement -= 8
+				bird_movement -= 5
 			
 			# if user presses spacebar while the game is not active
 			if event.key == pygame.K_SPACE and game_active == False:
 				game_active = True
 				pipe_list.clear()
-				bird_rect.center = (100, 384)
+				bird_rect.center = (50, height/2)
 				bird_movement = 0
 		
 		# custom event for spawning pipe
@@ -117,7 +111,7 @@ while True:
 		pass
 
 	# Floor
-	floor_x_position -= 1
+	floor_x_position -= 0.5
 	draw_floor()
 	if floor_x_position <= -width:
 		floor_x_position = 0
